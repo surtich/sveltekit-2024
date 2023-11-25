af8464fa5d11549e45ea10b6b779576b65067067
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-26 00:06:19 +0100

    Se almacena una cookie tras el "login".

--- src/routes/api/auth/login/+server.js
+++ src/routes/api/auth/login/+server.js
@@ -1,6 +1,6 @@
 import { error, json } from '@sveltejs/kit';
 
-export const POST = async ({ request }) => {
+export const POST = async ({ request, cookies }) => {
 	const data = await request.json();
 
 	if (!data.username) {
@@ -10,6 +10,10 @@ export const POST = async ({ request }) => {
 	if (!data.password) {
 		throw error(400, 'Password is required!');
 	}
+
+	cookies.set('token', 'token_Value', {
+		path: '/' // ya que sino lo fija en /api y sólo será válidas en esa ruta
+	});
 
 	return json(
 		{ name: data.username, id: 1 },
