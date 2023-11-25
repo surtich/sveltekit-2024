00c77d56178258e1e3a8ba914fc5bec18f4c47bd
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 01:48:58 +0100

    Usando [...] para capturar todas las rutas

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+
+	const { slug } = $page.params;
+	const paths = slug.split('/');
+</script>
+
+<h1>Viewing Docs For</h1>
+{#each paths as path}
+	<h2>{path}</h2>
+{/each}
