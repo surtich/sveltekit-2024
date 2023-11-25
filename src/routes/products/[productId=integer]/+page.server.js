61b5c3bfc9a854ce773eba668d85a2ab77103e23
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 19:21:12 +0100

    En vez de mostrar un error, otra opción es hacer una redirección.

--- src/routes/products/[productId=integer]/+page.server.js
+++ src/routes/products/[productId=integer]/+page.server.js
@@ -1,11 +1,11 @@
-import { error } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async (serverLoadEvent) => {
 	const { fetch, params } = serverLoadEvent;
 	const { productId } = params;
 	const response = await fetch(`http://localhost:4000/products/${productId}`);
 	if (response.status === 404) {
-		throw error(404, { message: 'Product not found', hint: 'Try a different product' });
+		throw redirect(307, '/products');
 	}
 	console.log('response', response.status);
 	const product = await response.json();
