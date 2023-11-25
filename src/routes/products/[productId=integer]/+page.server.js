c338ecce311efd9cc2517b6ce134419301ef1ae6
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 18:57:02 +0100

    Se cambia el error 500 por un 404 usando la función error de SvelteKit,

--- src/routes/products/[productId=integer]/+page.server.js
+++ src/routes/products/[productId=integer]/+page.server.js
@@ -1,14 +1,11 @@
-// import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 export const load = async (serverLoadEvent) => {
 	const { fetch, params } = serverLoadEvent;
 	const { productId } = params;
 	const response = await fetch(`http://localhost:4000/products/${productId}`);
 	if (response.status === 404) {
-		return {
-			status: 404,
-			error: new Error(`Product with id ${productId} not found`)
-		};
+		throw error(404, 'Product not found');
 	}
 	console.log('response', response.status);
 	const product = await response.json();
