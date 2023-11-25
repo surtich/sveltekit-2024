e47bc149d70c9feb8d9f3b7c57d9e7c286d8ac2a
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 11:57:38 +0100

    API: Modificando un comentario con un "endpoint patch"

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -7,3 +7,12 @@ export function GET({ params }) {
 	const comment = comments.find((comment) => comment.id === parseInt(commentId));
 	return json(comment);
 }
+
+export async function PATCH(requestEvent) {
+	const { params, request } = requestEvent;
+	const { commentId } = params;
+	const { text } = await request.json();
+	const comment = comments.find((comment) => comment.id === parseInt(commentId));
+	comment.text = text;
+	return json(comment);
+}
