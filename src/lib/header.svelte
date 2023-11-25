03ccd26a27ecd23b8f5e1dac7cee8895f8b68252
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 21:25:45 +0100

    Demostración de como funciona la precarga con valor "viewport".

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -2,18 +2,22 @@
 	import { page } from '$app/stores';
 </script>
 
+<div class="tall"></div>
 <svelte:head>
 	<title>{$page.data.title || 'SvelteKit demo'}</title>
 </svelte:head>
 
 <header class="layout-header">
 	<a href="/">Home</a>
-	<a href="/products" data-sveltekit-preload-code="hover">Products</a>
+	<a href="/products" data-sveltekit-preload-code="viewport">Products</a>
 	<a href="/address">Address</a>
 	<a href="/stocks">Stocks</a>
 </header>
 
 <style>
+	.tall {
+		height: 100vh;
+	}
 	a {
 		color: #e9c46a;
 		text-decoration: none;
