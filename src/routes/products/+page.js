66c0102b3ac7f976dd15f823b841be6594755ba8
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 18:37:58 +0100

    Loading: Los datos que sean sensibles o que procedan de una API externa que requiera un API_KEY se deben cargar en el lado del servidor (+page.server.js) y los datos no serializables se deben cargar en +page.js. Los datos cargados en +page.server.js son accesibles en +page.js y se pueden reenviar al cliente.

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -2,9 +2,6 @@ import Product from './product.svelte';
 
 export const load = async (loadEvent) => {
 	console.log('Load function called in page.js');
-	const { fetch } = loadEvent;
-	const title = 'List of available products';
-	const response = await fetch('http://localhost:4000/products');
-	const products = await response.json();
-	return { title, products, Component: Product };
+	const { data } = loadEvent;
+	return { ...data, Component: Product };
 };
