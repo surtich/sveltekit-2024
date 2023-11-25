a9c4893e9f71a5dc0f0c2f6fa782c01cd39990b4
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 22:16:45 +0100

    El comportamiento por defecto de SvelteKit es que la página primero se renderiza en el servidor y luego se vuelve a renderizar en el cliente en un proceso llamado "hidratación". Este segundo renderizado es necesario para que la página sea interactiva, ya que el servidor no puede ejecutar JavaScript.  En este commit se exportan las variables ssr y csr con valor true en el fichero "+page.js" del endpoint /products, que es el comportamiento por defecto. Al escribir en la barra de direcciones /products, se ven los mensajes de log tanto en el navegador como en el servidor.

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -1,7 +1,11 @@
 import Product from './product.svelte';
 
 export const load = async (loadEvent) => {
+	console.log('Load function called in page.js');
 	const { data } = loadEvent;
 	const notification = 'End of season sale!';
 	return { ...data, Component: Product, notification };
 };
+
+export const ssr = true;
+export const csr = true;
