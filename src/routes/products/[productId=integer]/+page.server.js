e9a5e35f735fa814a7785728d7de117646275497
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 18:50:54 +0100

    Implementando el componente "productDetails".

---
+++ src/routes/products/[productId=integer]/+page.server.js
@@ -0,0 +1,12 @@
+// import { redirect } from '@sveltejs/kit';
+
+export const load = async (serverLoadEvent) => {
+	const { fetch, params } = serverLoadEvent;
+	const { productId } = params;
+	const response = await fetch(`http://localhost:4000/products/${productId}`);
+	const product = await response.json();
+	const title = 'Product details';
+	return { title, product };
+};
+
+export const prerender = 'auto';
--- src/routes/products/[productId=integer]/+page.svelte
+++ src/routes/products/[productId=integer]/+page.svelte
@@ -1,7 +1,12 @@
 <script>
-	import { page } from '$app/stores';
-
-	let productId = $page.params.productId;
+	export let data;
+	const title = data.title;
+	const product = data.product;
 </script>
 
-<h1>Product {productId} details</h1>
+<h1>{title}</h1>
+
+<div>
+	<h2>{product.title} - ${product.price}</h2>
+	<p>{product.description}</p>
+</div>
