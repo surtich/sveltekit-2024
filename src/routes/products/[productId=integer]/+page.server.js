e0ffdbf4b64ae3b77633807a0d334979e75d6aee
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 18:55:14 +0100

    Se muestra un error 500 cuando se intenta mostrar los detalles d eun producto que no existe.

--- src/routes/products/[productId=integer]/+page.server.js
+++ src/routes/products/[productId=integer]/+page.server.js
@@ -4,6 +4,13 @@ export const load = async (serverLoadEvent) => {
 	const { fetch, params } = serverLoadEvent;
 	const { productId } = params;
 	const response = await fetch(`http://localhost:4000/products/${productId}`);
+	if (response.status === 404) {
+		return {
+			status: 404,
+			error: new Error(`Product with id ${productId} not found`)
+		};
+	}
+	console.log('response', response.status);
 	const product = await response.json();
 	const title = 'Product details';
 	return { title, product };
