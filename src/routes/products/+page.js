70fdd163e5b92f5231c055d8774554dc7427acd8
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 12:46:39 +0100

    Loading: Se muestra la lista de productos d ela base de datos

--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -1,4 +1,6 @@
 export const load = async () => {
 	const title = 'List of available products';
-	return { title };
+	const response = await fetch('http://localhost:4000/products');
+	const products = await response.json();
+	return { title, products };
 };
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,5 +1,16 @@
 <script>
 	export let data;
+	const { title, products } = data;
 </script>
 
-<h1>{data.title}</h1>
+<h1>{title}</h1>
+
+{#each products as product}
+	<div>
+		<a href={`/products/${product.id}`}>
+			<h2>{product.title}</h2>
+		</a>
+		<p>{product.description}</p>
+		<hr />
+	</div>
+{/each}
