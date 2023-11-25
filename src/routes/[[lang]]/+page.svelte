4f3ec8828d602bc6a0438a6806fe8df0fd0dd550
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 02:00:07 +0100

    Rutas y parámetros por defecto

---
+++ src/routes/[[lang]]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { page } from '$app/stores';
+
+	const { lang = 'en' } = $page.params;
+
+	const greetings = {
+		en: 'Hello!',
+		es: '¡Hola!',
+		fr: 'Bonjour!'
+	};
+
+	const greeting = greetings[lang];
+</script>
+
+<h1>{greeting}</h1>
