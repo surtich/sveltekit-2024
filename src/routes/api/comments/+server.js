95470b378fe2d6afd49ad74465e3be604a7dd6d9
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 11:42:00 +0100

    API: Se crea un nuevo comentario con un "endpoint POST"

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -4,3 +4,19 @@ import { comments } from '$lib/comments.js';
 export function GET() {
 	return json(comments);
 }
+
+export async function POST(requestEvent) {
+	const { request } = requestEvent;
+	const { text } = await request.json();
+	const newComment = {
+		id: comments.length + 1,
+		text
+	};
+	comments.push(newComment);
+	return new Response(JSON.stringify(newComment), {
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		status: 201
+	});
+}
