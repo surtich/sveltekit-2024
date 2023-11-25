cff92193497930469c5692ca96dff3c77a518c56
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 20:19:46 +0100

    Se prepara el siguiente ejercicio sobre /stoks: Se mueve +page.js y +page.svelte a +layout.js +layout.svelte respectivamente.

---
+++ src/routes/stocks/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let data;
+</script>
+
+<br />
+<span>Most active stock - {data.mostActiveStock.symbol}</span>
+<span>Top gaining stock - {data.topGainingStock.symbol}</span>
+<span>Top losing stock - {data.topLosingStock.symbol}</span>
+<slot />
+
+<style>
+	span {
+		margin-right: 30px;
+		font-size: 24px;
+	}
+</style>
--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -1,15 +1,1 @@
-<script>
-	export let data;
-</script>
-
-<br />
-<span>Most active stock - {data.mostActiveStock.symbol}</span>
-<span>Top gaining stock - {data.topGainingStock.symbol}</span>
-<span>Top losing stock - {data.topLosingStock.symbol}</span>
-
-<style>
-	span {
-		margin-right: 30px;
-		font-size: 24px;
-	}
-</style>
+<h1>Actively trading stocks</h1>
