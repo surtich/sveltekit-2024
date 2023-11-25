9fd84bdd2b8ec9323195a0f4a836a2425fa59f87
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 19:29:23 +0100

    Cargando datos del servidor en un componente +layout.svelte.

---
+++ src/routes/products/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let data;
+	const title = data.title;
+	const featuredProducts = data.featuredProducts;
+</script>
+
+<slot />
+
+<h2>{title}</h2>
+
+{#each featuredProducts as product}
+	<div class="product-item">
+		<h2>{product.title}</h2>
+		<p>{product.description}</p>
+	</div>
+{/each}
+
+<style>
+	.product-item {
+		display: inline-block;
+		width: 200px;
+		/* height: 100vh; */
+		margin: 10px;
+		border: 1px solid #ccc;
+		padding: 10px;
+	}
+</style>
--- src/routes/products/[productId=integer]/+layout.svelte
+++
@@ -1,2 +0,0 @@
-Featured product
-<slot />
