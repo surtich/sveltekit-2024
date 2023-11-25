905a29b3f038a288f62c247e68f8daf97f74977b
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 22:49:37 +0100

    Las API también se pueden prerenderizar. En este ejemplo se crea la API current-time prerenderizada. Se comprueba que la hora se queda grabada en el directorio "prerendered" y que al navegar a /api/current-time la hora no se actualiza.

---
+++ src/routes/api/current-time/+server.js
@@ -0,0 +1,6 @@
+export async function GET() {
+	console.log('GET function called in server.js of current-time API');
+	return new Response(new Date().toLocaleTimeString());
+}
+
+export const prerender = true;
