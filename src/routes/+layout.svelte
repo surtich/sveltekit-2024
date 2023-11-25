14c4a7b0cb2f1ced77e7b46eccd50e05daf49449
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-25 03:10:18 +0100

    Moviendo el "header" y el "footer" a componentes Svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,8 @@
-<header class="layout-header">Header</header>
+<script>
+	import Footer from './footer.svelte';
+	import Header from './header.svelte';
+</script>
+
+<Header />
 <slot />
-<footer class="layout-footer">Footer</footer>
-
-<style>
-	.layout-header {
-		background-color: #264653;
-		color: #f4a261;
-		font-size: 30px;
-		text-align: center;
-		padding: 10px;
-	}
-
-	.layout-footer {
-		background-color: #264653;
-		color: #e9c46a;
-		font-size: 30px;
-		text-align: center;
-		padding: 10px;
-	}
-</style>
+<Footer />
