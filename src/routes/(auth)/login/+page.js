510433d5fb964ee9fdf032d72985609983ee0ec4
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-26 01:21:26 +0100

    Se redirige a / después de login

--- src/routes/(auth)/login/+page.js
+++ src/routes/(auth)/login/+page.js
@@ -4,6 +4,6 @@ export const load = async ({ parent }) => {
 	const { username } = await parent();
 
 	if (username) {
-		//throw redirect(307 /*temporal redirect */, '/');
+		throw redirect(303 /*temporal redirect */, '/');
 	}
 };
