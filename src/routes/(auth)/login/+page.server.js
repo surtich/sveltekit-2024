2f26fe4ba8d5cebd22c5023b3b6dfa2a6436978a
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-26 01:16:53 +0100

    login con form actions.

--- src/routes/(auth)/login/+page.server.js
+++ src/routes/(auth)/login/+page.server.js
@@ -1,6 +1,21 @@
+import { fail } from '@sveltejs/kit';
+
 export const actions = {
-	async default({ request }) {
+	async default({ request, cookies }) {
 		const form = await request.formData();
-		console.log(form.get('username'), form.get('password'));
+		const username = form.get('username');
+		const password = form.get('password');
+
+		if (!username) {
+			return fail(400, { usernameMissing: true });
+		}
+
+		if (!password) {
+			return fail(400, { passwordMissing: true });
+		}
+
+		cookies.set('token', 'token_value', { path: '/' });
+
+		return { success: true };
 	}
 };
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,12 +1,31 @@
+<script>
+	import { page } from '$app/stores';
+	export let form;
+
+	$: console.log('form', form); // accesible solo en /login
+	$: console.log('page', $page.form, $page.status); // accesible en cualquier componente
+</script>
+
+{#if form?.success}
+	<p style="color: green;">You are now logged in!</p>
+{/if}
+
 <form method="POST">
 	<label for="username">Username</label><br />
 	<input type="text" name="username" id="username" placeholder="Username" />
-
-	<br /><br />
+	<br />
+	{#if form?.usernameMissing}
+		<p style="color: red;">Username is required!</p>
+	{/if}
+	<br />
 
 	<label for="password">Password</label><br />
 	<input type="text" name="password" id="password" placeholder="Password" />
 
-	<br /><br />
+	<br />
+	{#if form?.passwordMissing}
+		<p style="color: red;">Password is required!</p>
+	{/if}
+	<br />
 	<button type="submit">Submit</button>
 </form>
