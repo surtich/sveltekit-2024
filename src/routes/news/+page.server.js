493a2ac00b491cdd2ba3df965c417bc68c777457
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-26 01:42:06 +0100

    Si se intenta navegar a /news sin estar conectado, redirige a /login.

--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,4 +1,11 @@
-export const load = () => {
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ parent }) => {
+	const { username } = await parent();
+
+	if (!username) {
+		throw redirect(303 /*temporal redirect */, '/login');
+	}
 	const newsAPIKey = 'YOUR_NEWS_API_KEY';
 	console.log(newsAPIKey);
 	/** Make API call using API key to fetch news data */
